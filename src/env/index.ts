93f178ca5712cdd267e8cb8a0babd089f6f2b7b1
João Victor Silva <joaovictorsilva.dev@gmail.com>
2023-12-19 18:22:40 -0400

    eslint config

---
+++ src/env/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import z from 'zod'
+const envSchema = z.object({
+    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'), // o enum() define que o valor tem que ser UM dos fornecidos
+    PORT: z.coerce.number().default(3333) // o coerce() converte para number, caso o valor seja uma string
+})
+
+const _env= envSchema.safeParse(process.env) //o safeParse compara o schema pre-definido com algum desejado, e confirma se a estrutura está igual
+
+
+if(_env.success === false){
+    console.error("Invalid Environment Variables", _env.error.format())
+    // Caso exista algum erro, ele não deixa a apllicação rodar
+    throw new Error('Invalid Environment Variables.')
+
+}
+
+export const env = _env.data
+
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import app from './app'
-
+import app from './app';
+import { env } from './env';
 
 app.listen({
     host: '0.0.0.0',
-    port: 3000
-}).then(()=> console.log('HTTP Server Running.'))
+    port: env.PORT
+}).then(()=> console.log('HTTP Server Running.'));
