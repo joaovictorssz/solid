13b0f662c496cf6fd6643c2c58d6ff1100676568
João Victor Silva <joaovictorsilva.dev@gmail.com>
2023-12-20 00:29:21 -0400

    first create of user endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,29 @@
 import {fastify} from 'fastify'
-import { PrismaClient } from 'prisma/prisma-client'
+import { prisma } from './lib/prisma';
+import {z} from 'zod'
+
 const app = fastify()
 
-const prisma = new PrismaClient()
+app.post('/user', async (request, reply)=>{
+    
+    const RequestBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password_hash: z.string().min(6)
+    })
+
+    const { email, name, password_hash } = RequestBodySchema.parse(request.body)
+
+    await prisma.user.create({
+        data: {
+            email,
+            name,
+            password_hash
+        }
+    })
+    return reply.status(201).send()
+})
+
 
 
 
--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,4 @@ import { env } from './env';
 app.listen({
     host: '0.0.0.0',
     port: env.PORT
-}).then(()=> console.log('HTTP Server Running.'));
+}).then(()=> console.log('HTTP Server Running  at ', env.PORT));
