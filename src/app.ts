0b5b68eceac1a66a28c82f852080c6252131c7d6
João Victor Silva <joaovictorsilva.dev@gmail.com>
2023-12-26 17:35:44 -0400

    global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
 import { fastify } from 'fastify'
 import { userRoutes } from './http/routes/user';
+import { ZodError } from 'zod';
+import { env } from './env';
 
 const app = fastify()
 
 app.register(userRoutes)
+
+app.setErrorHandler((error, _, reply)=>{
+    if(error instanceof ZodError){
+        return reply.status(400).send({message: "Validation error.", issues: error.format()})
+    }
+
+    if(env.NODE_ENVIRONMENT !== 'production'){
+        console.error(error)
+    }
+
+    return reply.status(500).send({message: "Internal server error."})
+})
 
 
 export default app; 
