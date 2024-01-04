edd6984a4fff23be88b46d4cf58de882015e7b09
João Victor Silva <joaovictorsilva.dev@gmail.com>
2024-01-04 14:18:34 -0400

    authentication test e2e

--- src/http/controllers/user/authentication.spec.ts
+++ src/http/controllers/user/authentication.spec.ts
@@ -1,7 +1,43 @@
-import 'dotenv/config'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import app from '@/app'
 
-import { test } from "vitest";
+describe('Authentication Controler E2E', ()=>{
 
-test('test', ()=>{
-    console.log(process.env.DATABASE_URL)
+    beforeAll(async ()=>{
+        await app.ready()
+
+         // create an user
+         await request(app.server).post('/register').send({
+            email: 'johndoe@example.com',
+            name: 'John Doe',
+            password: '123456'
+        })
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('should be able to authenticate', async () => {
+
+        const response = await request(app.server).post('/auth').send({
+            email: 'johndoe@example.com',
+            password: '123456'
+        })
+
+        expect(response.statusCode).toEqual(200)
+    })
+
+    it('should not be able to authenticate with invalid credentials', async () => {
+        const response = await request(app.server).post('/auth').send({
+            email: 'johndoe@example.com',
+            password: 'invalid-password'
+        })
+
+        expect(response.statusCode).toEqual(401)
+        expect(response.body.message).toBe('Invalid credentials.')
+
+        
+    })
 })
